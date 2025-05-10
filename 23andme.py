df7ef944023b0526bfac91a4fb236580a0d0a223
Shareen Arshad <107083486+shareenarshad-io@users.noreply.github.com>
2025-05-10 13:15:03 -0400

    finished code

--- 23andme.py
+++ 23andme.py
@@ -39,10 +39,85 @@ plt.show()
 
 #It looks like there has been a sudden change in daily sales. What date did it occur?
 
+# compare sales amount difference with the previous day and find the biggest difference 
+# which should give the sudden change in the plot
+daily_sales_df['prev_day_sale_amount'] = [None] + daily_sales_df['sales_amount'].to_list()[:-1]
+daily_sales_df['difference_btw_prev_day'] = daily_sales_df['sales_amount'] - daily_sales_df['prev_day_sale_amount']
+daily_sales_df.head()
 
-
-
+# get the index of max change to answer what date did it occur
+print(daily_sales_df['difference_btw_prev_day'].idxmax())
+daily_sales_df[daily_sales_df.index == '2013-04-29']
 
 #Is the change in daily sales at the date you selected statistically significant? If so, what is the p-value?
+
+
+# scipy.stats include a function to do t-test and obtain p-value
+from scipy import stats
+
+# first drop all nan values (if any) for the test 
+daily_sales_df.dropna(inplace=True)
+
+# then compare the max change with all sales_amount column 
+# to understand its statistical importance
+stats.ttest_1samp(daily_sales_df['sales_amount'], 732)
+
+#yes it is. The p-value can be seen from this code. 
+
 #Does the data suggest that the change in daily sales is due to a shift in the proportion of male-vs-female customers? Please use plots to support your answer (a rigorous statistical analysis is not necessary).
+
+# create gender_portions_df to analyze sales distribution over genders
+df['sale_day'] = pd.to_datetime(df['sale_time']).dt.floor('d')
+gender_portions_df = df.groupby(['sale_day', 'purchaser_gender']).size().reset_index(name='sales_amount')
+
+print(gender_portions_df)
+
+# calculate female and male sales amount separately
+new_df = pd.DataFrame(gender_portions_df['sale_day'].unique(),columns=['sale_day'])
+new_df['female_sales_amount'] = pd.Series(gender_portions_df[gender_portions_df.purchaser_gender == 'female']['sales_amount']).reset_index(drop=True)
+new_df['male_sales_amount'] = pd.Series(gender_portions_df[gender_portions_df.purchaser_gender == 'male']['sales_amount']).reset_index(drop=True)
+new_df[-5:]
+
+# calculate gender ratio of sales
+new_df['gender_ratio'] = new_df['male_sales_amount']/new_df['female_sales_amount']
+
+# plot gender ratio change over 50 weeks with the total sales amount
+# observe if there is a correlation between them
+plot_df = new_df.copy()
+plot_df['total_sales_amount']  = plot_df['male_sales_amount'] + plot_df['female_sales_amount']
+plot_df.drop(columns=['female_sales_amount', 'male_sales_amount'], inplace=True)
+plot_df.index = plot_df['sale_day']
+plot_df.drop(columns=['sale_day'], inplace=True)
+plot_df.plot(secondary_y = ['gender_ratio'],figsize=FIG_SIZE, title= "Gender ratio and sales amount over all 50 weeks")
+plt.show()
 #Assume a given day is divided into four dayparts: night (12:00AM - 6:00AM), morning (6:00AM - 12:00PM), afternoon (12:00PM - 6:00PM) and evening (6:00PM - 12:00AM). What is the percentage of sales in each daypart over all 50 weeks?
+
+# extract sale hour to divide sales to the part of days
+df['sale_hour']  = pd.to_datetime(df['sale_time']).dt.ceil('h').dt.hour
+df.dtypes
+
+# define a function to apply sale hour column to decide a part of day based on the mentioned logic
+def assign_day_part(sale_hour):
+    """Assign labels based on sale hour."""
+    if sale_hour >= 6 and sale_hour < 12:
+        return 'morning'
+    if sale_hour >= 12 and sale_hour < 18:
+        return 'afternoon'
+    if sale_hour >= 18 and sale_hour < 24:
+        return 'evening'
+    if sale_hour >= 0 and sale_hour < 6:
+        return 'night'
+    
+# apply the defined function and create day_part column
+df['day_part'] = df['sale_hour'].apply(assign_day_part)
+
+# drop unnecessary columns
+df.drop(columns = ['purchaser_gender', 'sale_hour', 'sale_time'], inplace= True)
+
+# calculate sales amount per day part
+df_pct_sales = df.groupby(df.day_part).size().reset_index(name='day_part_sales_amount')
+
+# calculate sales amount percentage per day part
+df_pct_sales['day_part_sales_percentage'] = df_pct_sales['day_part_sales_amount'] / df_pct_sales['day_part_sales_amount'].sum()
+
+print(df_pct_sales)
